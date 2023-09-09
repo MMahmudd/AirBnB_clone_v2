c2b96c3611afe9954e9fdd709b29eaf3e03be8e7
MMahmudd <shametlab@gmail.com>
2023-09-09 08:31:23 -0700

    update tasks

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['18.207.141.252', '54.157.159.85']
 
 
 def do_pack():
