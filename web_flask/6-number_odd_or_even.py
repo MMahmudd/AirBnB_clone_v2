a6bfd04d610bff523f35422d9f2a78eb8f9e0569
MMahmudd <shametlab@gmail.com>
2023-09-22 05:57:56 -0700

    update tasks

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,55 +1,52 @@
 #!/usr/bin/python3
-"""
-A_Flask web_application with_routes_for_Task 6.
-"""
-
-from flask import Flask, render_template
-from flask import abort
-
+"""script_that_starts_a Flask web_application"""
+from flask import Flask, escape, render_template
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Display 'Hello HBNB!' when_accessing_the_root URL."""
+def route():
+    """Return_two_words"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Display 'HBNB' when_accessing_the /hbnb URL."""
+def route_hbnb():
+    """Return_a_word"""
     return "HBNB"
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """Display 'C ' followed_by_the_value of_the text_variable."""
-    return "C " + text.replace('_', ' ')
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text):
-    """Display 'Python ' followed_by_the value_of the text_variable."""
-    return "Python " + text.replace('_', ' ')
+@app.route('/c/<path:subpath>', strict_slashes=False)
+def route_c(subpath):
+    """Return_subpath"""
+    return "C {}".format(escape(subpath).replace('_', ' '))
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def is_number(n):
-    """Display 'n is_a_number' only_if n is_an_integer."""
-    return "{} is a number".format(n)
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """Display_an HTML page_with 'Number: n' if_n is_an_integer."""
-    if isinstance(n, int):
-        return render_template('6-number.html', n=n)
-    else:
-        abort(404)
+@app.route('/python', defaults={'subpath': 'is cool'}, strict_slashes=False)
+@app.route('/python/<path:subpath>', strict_slashes=False)
+def route_python(subpath):
+    """Return_subpath"""
+    return "Python {}".format(escape(subpath).replace('_', ' '))
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    """Display_an HTML page_indicating_if n is_even_or_odd."""
-    if isinstance(n, int):
-        odd_or_even = "even" if n % 2 == 0 else "odd"
-        return render_template('6-number_odd_or_even.html', n=n, odd_or_even=odd_or_even)
-    else:
-        abort(404)
 
-if __name__ == "__main__":
+@app.route('/number/<int:num>', strict_slashes=False)
+def route_number(num):
+    """Return_only_if num_is_a_int"""
+    return "{} is a number".format(escape(num))
+
+
+@app.route('/number_template/<int:num>', strict_slashes=False)
+def route_template(num):
+    """Return a_file HTML only_if_num is_a_int"""
+    return render_template("5-number.html", n=num)
+
+
+@app.route('/number_odd_or_even/<int:num>', strict_slashes=False)
+def route_odd_even(num):
+    """Retrieves_an HTML file_only if_num_is an_int and_describes
+    if num is_odd or_even"""
+    return render_template("6-number_odd_or_even.html", n=num)
+
+
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
