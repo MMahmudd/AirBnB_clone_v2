1094c5b8acadfcd56bf2559115fe774080e20d2e
MMahmudd <shametlab@gmail.com>
2023-10-21 16:35:07 +0000

    update taks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,17 +2,17 @@
 """
 A simple_Flask web_application.
 """
-
+# Import_necessary_modules
 from flask import Flask
 
+# Create a Flask application instance
 app = Flask(__name__)
 
-# comments
-@app.route('/airbnb-onepage/', strict_slashes=False)
-def hello_hbnb():
-    """Display 'Hello HBNB!' when_accessing_the root URL."""
+# Define_a_route that_serves content_for "/airbnb-onepage/"
+@app.route('/airbnb-onepage/', methods=['GET'])
+def hello_airbnb():
     return "Hello HBNB!"
 
-
 if __name__ == "__main__":
+    # Run_the Flask_application_on port_5000
     app.run(host="0.0.0.0", port=5000)
